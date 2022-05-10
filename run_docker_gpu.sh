41db72f1846fc94c13af27410b806b050ae50e0b
theia <charbel-a-h@outlook.com>
2022-05-10 23:20:06 +0200

    Working CGAN with Skeleton for CycleGAN

--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --rm --runtime=nvidia --gpus all -v ${PWD}:/ups-mv-gans-project -p 8000:8000 ups-gans bash
+docker run -it --rm --runtime=nvidia --gpus all -v ${PWD}:/ups-mv-gans-project -p $1:$1 ups-gans bash
 
